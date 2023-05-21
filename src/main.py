56f338139a3e0de482998aee5d8d399d392953b4
jakub.prusakiewicz <jakub.prusakiewicz@silenteight.com>
2023-05-21 22:29:55 +0200

    improvements

--- src/main.py
+++ src/main.py
@@ -2,14 +2,13 @@ import sys
 from omegaconf import OmegaConf
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MinMaxScaler
 
 sys.path.append('src')
 
 from read_data import read_data, get_split_values
 from train import build_model
 from evaluate import evaluate_model
-from preprocess import get_feature_selector, get_preprocessor
+from preprocess import get_feature_selector, get_preprocessor, get_balancer
 
 
 # todo kuba save artifacts (config, model, metrics, etc.) in one place
@@ -19,11 +18,9 @@ def run() -> dict:
     x, y = get_split_values(data)
     x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, **config.train_test_split)
 
-    preprocessor = get_preprocessor(config.preprocessing)
-    # pipeline order: rescale, reduce features, balance, train
-    pipeline = make_pipeline(preprocessor,
-                             MinMaxScaler(),
+    pipeline = make_pipeline(get_preprocessor(config.preprocessing),
                              get_feature_selector(config.feature_selection),
+                             # todo balance data using get_balancer() (if wont work, use https://imbalanced-learn.org/stable/references/generated/imblearn.pipeline.Pipeline.html)
                              build_model(config.model), verbose=2)
     pipeline.fit(x_train, y_train)
 
--- src/preprocess.py
+++ src/preprocess.py
@@ -2,35 +2,21 @@ import numpy as np
 
 from sklearn.svm import LinearSVC
 from sklearn.feature_selection import SelectFromModel
-import numpy as np
-
 from sklearn.compose import ColumnTransformer
-from sklearn.datasets import fetch_openml
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.feature_selection import SelectPercentile, chi2
-
-
-def preprocess(data: np.array, config) -> np.array:
-    # todo
-    return data
-
-
-def rescale_data(data: np.array, config) -> np.array:
-    # from sklearn.preprocessing import MinMaxScaler
-    # todo will we need to scale x_test as well?
-    pass
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler
+from sklearn.feature_selection import VarianceThreshold, SelectPercentile, chi2
 
 
 def get_feature_selector(config):
-    # from sklearn.feature_selection import VarianceThreshold
-    return SelectFromModel(LinearSVC(**config.LinearSVC))
+    # todo use python switch case mechanism https://docs.python.org/3/whatsnew/3.10.html#pep-634-structural-pattern-matching
+    # return VarianceThreshold # todo add as option
+    if config.type == "LinearSVC":
+        return SelectFromModel(LinearSVC(**config.params))
 
 
-def balance_data(data: np.array, config) -> np.array:
+def get_balancer(data: np.array, config) -> np.array:
     # use one of belows options to balance data (create switch case based on config)
     # python switch case mechanism https://docs.python.org/3/whatsnew/3.10.html#pep-634-structural-pattern-matching
     # 1. from sklearn.decomposition import PCA
@@ -43,27 +29,38 @@ def balance_data(data: np.array, config) -> np.array:
     pass
 
 
+def get_scaler(config):
+    return MinMaxScaler()
+    # return StandardScaler() # todo add as option
+
+
 def get_preprocessor(config):
     # numeric_features = config.numeric_features
     # categorical_features = config.categorical_features
-    numeric_features = [1, 5, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23] # todo to config
-    categorical_features = [2, 3, 4, 6, 7, 8, 9, 10, 11] # todo to config
+    numeric_features = [1, 5, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23]  # todo move to config
+    categorical_features = [2, 3, 4, 6, 7, 8, 9, 10, 11]  # todo move to config
     # todo 0 is index column, if data will be read without index, all these must be -1
 
     numeric_transformer = Pipeline(
-        steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
+        # todo if list will be empty, (nothing in config) it will fail
+        steps=[
+            ("imputer", SimpleImputer(strategy="median")),  # todo choose in config
+            ("scaler", get_scaler(config.scaler))  # todo choose in config
+        ]
     )
 
     categorical_transformer = Pipeline(
+        # todo if list will be empty, (nothing in config) it will fail
         steps=[
-            ("encoder", OneHotEncoder(handle_unknown="ignore")),
-            ("selector", SelectPercentile(chi2, percentile=50)),
+            ("encoder", OneHotEncoder(handle_unknown="ignore")),  # todo choose in config ()
+            ("selector", SelectPercentile(chi2, percentile=50)),  # todo choose in config
         ]
     )
     preprocessor = ColumnTransformer(
+        # todo if list will be empty, (nothing in config) it will fail
         transformers=[
-            ("num", numeric_transformer, numeric_features),
-            ("cat", categorical_transformer, categorical_features),
+            ("num", numeric_transformer, numeric_features),  # todo choose in config
+            ("cat", categorical_transformer, categorical_features),  # todo choose in config
         ]
     )
     return preprocessor
